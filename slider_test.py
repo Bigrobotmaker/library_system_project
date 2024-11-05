df9bb86d82faabf06e5ef4632ab987c7b4f1baf9
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-11-05 11:19:52 +0000

    Update slider_test.py

--- slider_test.py
+++ slider_test.py
@@ -1,16 +1,17 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.slider import Slider
 class application(App):
     def build(self):
-        layout = GridLayout(cols = 1)
+        layout = BoxLayout(orientation = 'vertical')
         s = Slider(min=-100, max=100, value=25)
         for i in range (0,10):
-            layout.add_widget(Button(text = 'button'))
+            layout.add_widget(Button(text = 'button')(pos_hint=('right':i)))
         layout.add_widget(s)
+        return layout
 if __name__ == '__main__':
    myApp = application()
    myApp.run()
