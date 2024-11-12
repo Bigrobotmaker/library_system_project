36f41e5a17e42f16d135feb20ee95f69403cbde2
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-11-12 10:25:39 +0000

    Update slider_test.py

--- slider_test.py
+++ slider_test.py
@@ -8,8 +8,9 @@ class application(App):
     def build(self):
         layout = BoxLayout(orientation = 'vertical')
         s = Slider(min=-100, max=100, value=25)
-        for i in range (0,10):
-            layout.add_widget(Button(text = 'button')(pos_hint=('right':i)))
+        layout.add_widget(Button(text = 'useless button'))
+        layout.add_widget(Button(text = 'button', pos_hint={'right': 1}))
+        layout.add_widget(Button(text = 'button', pos_hint={'right': 2}))
         layout.add_widget(s)
         return layout
 if __name__ == '__main__':
