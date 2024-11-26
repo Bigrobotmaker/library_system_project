75774b881bdc162053125df73c2ab704bca6fa55
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-11-26 11:05:47 +0000

    did stuff

--- project_no_gui.py
+++ project_no_gui.py
@@ -1,12 +1,26 @@
 import sqlite3
 def addbook(title, author, genre, id, copies):
-    cursor.execute('INSERT INTO availablebooks VALUES ("' + title + '", "' + author + '", "' + genre + '", "' + id + '", "' + copies + '")')
-connection = sqlite3.connect("Currentinventory.db")
+    inuse = False
+    cursor.execute('SELECT id FROM availablebooks')
+    taken = cursor.fetchall()
+    for used in taken:
+        if ','.join(used) == id:
+            inuse = True
+    if inuse == True:
+        print("invalid id (already in use)")
+    else:
+        cursor.execute('INSERT INTO availablebooks VALUES ("' + title + '", "' + author + '", "' + genre + '", "' + id + '", "' + copies + '")')
+        connection.commit()
+def removebook(id):
+    cursor.execute('DELETE FROM availablebooks\nWHERE id = "' + id + '"')
+    connection.commit()
+connection = sqlite3.connect("Currentinventorytest.db")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS availablebooks (title TEXT, author TEXT, genre TEXT, id TEXT, copies TEXT)")
 cursor.execute("CREATE TABLE IF NOT EXISTS borrowedbooks (title TEXT, author TEXT, genre TEXT, id TEXT, copies TEXT, loanedto TEXT, due TEXT)")
-addbook('one piece', 'oda', 'action manga', '0', '1')
-cursor.execute('SELECT * FROM availablebooks\nWHERE title = "' + 'one piece' + '"')
+addbook('one piece vol 2', 'oda', 'action manga', '1', '1')
+cursor.execute('SELECT * FROM availablebooks')#\nWHERE title = "' + 'one piece' + '"')
 rows = cursor.fetchall()
 for row in rows:
     print(row)
+connection.close()
