f79ffa1de887f4ac636a513c361dfc4c47f821f0
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-11-26 12:08:46 +0000

    something I guess

--- project_no_gui.py
+++ project_no_gui.py
@@ -5,7 +5,7 @@ def addbook(title, author, genre, id, copies):
     taken = cursor.fetchall()
     for used in taken:
         if ','.join(used) == id:
-            inuse = True
+           inuse = True
     if inuse == True:
         print("invalid id (already in use)")
     else:
@@ -19,10 +19,10 @@ def addcopy(id):
     book = cursor.fetchall()
 connection = sqlite3.connect("Currentinventorytest.db")
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS availablebooks (title TEXT, author TEXT, genre TEXT, id TEXT, copies TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS availablebooks (title TEXT, author TEXT, genre TEXT, id TEXT NOT NULL PRIMARY KEY, copies TEXT)")
 cursor.execute("CREATE TABLE IF NOT EXISTS borrowedbooks (title TEXT, author TEXT, genre TEXT, id TEXT, copies TEXT, loanedto TEXT, due TEXT)")
 addbook('one piece vol 2', 'oda', 'action manga', '1', '1')
-cursor.execute('SELECT * FROM availablebooks')#\nWHERE title = "' + 'one piece' + '"')
+cursor.execute('SELECT * FROM availablebooks')
 rows = cursor.fetchall()
 for row in rows:
     print(row)
