8e7793d8b8bfe41c10ce38e66731ccc0308d6362
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2025-01-28 12:00:50 +0000

    borrowid implimented

--- databasefunctions.py
+++ databasefunctions.py
@@ -45,17 +45,24 @@ def register(u,p,c):
 def borrow(id, date, user):
     connection = sqlite3.connect("Testinventory.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS Borrowed (title TEXT NOT NULL, author TEXT NOT NULL, genre TEXT NOT NULL, id TEXT NOT NULL PRIMARY KEY, user TEXT NOT NULL, due TEXT NOT NULL)")
+    idcursor = connection.cursor
+    cursor.execute("CREATE TABLE IF NOT EXISTS Borrowed (title TEXT NOT NULL, author TEXT NOT NULL, genre TEXT NOT NULL, id TEXT NOT NULL, user TEXT NOT NULL, due TEXT NOT NULL, borrowid TEXT NOT NULL PRIMARY KEY)")
     try:
         cursor.execute('SELECT title, author, genre, copies FROM inventory\nWHERE id = "' + id + '"')
         info = cursor.fetchall()
         title = (info[0])[0]
         author = (info[0])[1]
         genre = (info[0])[2]
+        idcursor.execute('SELECT * FROM Borrowed')
+        borrowed = idcursor.fetchall
+        borrowid = 0
+        for i in borrowed:
+            borrowid = borrowid + 1
         if int((info[0])[3]) > 0:
-            cursor.execute('INSERT INTO borrowed VALUES ("' + title + '", "' + author + '", "' + genre + '", "' + id + '", "' + user + '", "' + date + '")')
+            cursor.execute('INSERT INTO borrowed VALUES ("' + title + '", "' + author + '", "' + genre + '", "' + id + '", "' + user + '", "' + date + '", "' + str(borrowid) + '")')
             cursor.execute('UPDATE inventory SET copies ="' + str(int((info[0])[3]) - 1) + '" WHERE id = "' + id + '"')
             connection.commit()
+            connection.close()
             return("borrowing successful")
         else:
             return("no copies available, try again later")
